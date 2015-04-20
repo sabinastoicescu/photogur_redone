5af961dd76474d5e11cc2974357ff2f458a8ae7c
Sabina <sabina_stoicescu@yahoo.com>
2015-04-19 22:45:30 -0400

    finalized

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+   root 'pictures#index'
+ resources :pictures
 
-  get 'pictures' => 'pictures#index'
-  get 'pictures/:id' => 'pictures#show', as: 'picture' 
-
+ get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
